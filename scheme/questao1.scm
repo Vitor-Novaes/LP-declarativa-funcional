37194bb60c79428c8698f3e9561dab74b3757048
Victor Wilker dos Santos de Oliveira <wilkersantos125@gmail.com>
2018-11-13 20:06:25 -0300

    1º e 2º questão da linguagem funcional

---
+++ scheme/questao1.scm
@@ -0,0 +1,7 @@
+(define vol(lambda (raio)
+    (/ (* 4 3.14 (* raio raio raio)) 3)
+));<- definindo função 
+
+(print " ------ volume da esfera em função do raio ------" )
+(print "Digite o valor do raio: ")
+(display (vol (read)));chamda da função com leitura do raio
---
+++ scheme/questao2.scm
@@ -0,0 +1,5 @@
+(define expo(lambda (x y)( expt y x)));<- definindo função 
+
+(print " ------ Exponenciar x em y ------" )
+(print "Digite o valor de x e depois o de y ")
+(display (expo (read)  (read)));chamda da função com leitura de x e y
